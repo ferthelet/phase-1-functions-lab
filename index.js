9cd2e9fc416a1dccd07c641bbf589ea0fd2c45ac
Juan Salazar <ferthelet@gmail.com>
2023-03-24 00:59:58 -0500

    fixes distance in feet

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ function distanceFromHqInBlocks(block) {
     return Math.abs(42 - block);
 }
 
-function distanceFromHqInFeet(blocks) {
+
+function distanceFromHqInFeet(block) {
     // translates the number of blocks from headquarters to the
     // distance in feet. Each block in Manhattan is 264 feet long.
-    return blocks * 264;
+    return distanceFromHqInBlocks(block) * 264;
 }
 
 function distanceTravelledInFeet(startBlock, destBlock) {
     // takes in both the start and destination blocks, and returns
     // the number of feet travelled.
-    return distanceFromHqInFeet((distanceFromHqInBlocks(startBlock))) - 
-    distanceFromHqInFeet((distanceFromHqInBlocks(startBlock)));
+    return  distanceFromHqInFeet((startBlock)) +
+            distanceFromHqInFeet((destBlock));
 }
 
+console.log(distanceFromHqInFeet(43) );
